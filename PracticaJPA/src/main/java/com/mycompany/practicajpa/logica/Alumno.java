2375bc30940648d384b2049d69b415dd84f11488
FabricioPuccio <fabriciopuccio@yahoo.com>
2023-11-08 11:27:04 -0300

    feat/ se crea entidad Alumno, constructores, getters y setters y su correspondiente mapeo con anotaciones

---
+++ PracticaJPA/src/main/java/com/mycompany/practicajpa/logica/Alumno.java
@@ -0,0 +1,103 @@
+package com.mycompany.practicajpa.logica;
+
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Alumno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Basic
+    private String nombre;
+    private String apellido;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+
+    public Alumno() {
+
+    }
+
+    public Alumno(String nombre, String apellido, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Alumno(int id, String nombre, String apellido, Date fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String toString() {
+        return "\nAlumno id: " + id + "| Nombre: " + nombre + " Apellido: " + apellido + " Fecha de nacimiento: " + fechaNacimiento + "";
+    }
+
+}
