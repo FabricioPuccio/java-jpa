cf20d56959728a604b473dcc5242df99c535a110
FabricioPuccio <fabriciopuccio@yahoo.com>
2023-11-09 09:31:24 -0300

    feat/ constructor de ControladoraPersistencia con instancia de AlumnoJpaController

---
+++ PracticaJPA/src/main/java/com/mycompany/practicajpa/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,15 @@
+package com.mycompany.practicajpa.persistencia;
+
+/**
+ *
+ * @author fabri
+ */
+public class ControladoraPersistencia {
+
+    AlumnoJpaController ajc;
+
+    public ControladoraPersistencia() {
+        this.ajc = new AlumnoJpaController();
+    }
+
+}
