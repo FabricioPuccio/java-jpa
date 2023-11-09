fff6b4453ab12a82916030d2b29570c414d40e91
FabricioPuccio <fabriciopuccio@yahoo.com>
2023-11-09 14:17:33 -0300

    feat/ creamos clase ControladoraLogica siguiendo el modelo de capas y el método createAlumno

--- PracticaJPA/src/main/java/com/mycompany/practicajpa/PracticaJPA.java
+++ PracticaJPA/src/main/java/com/mycompany/practicajpa/PracticaJPA.java
@@ -1,10 +1,13 @@
 package com.mycompany.practicajpa;
 
-import com.mycompany.practicajpa.persistencia.ControladoraPersistencia;
+import com.mycompany.practicajpa.logica.Alumno;
+import com.mycompany.practicajpa.logica.ControladoraLogica;
+import java.util.Date;
 
 public class PracticaJPA {
 
     public static void main(String[] args) {
-        ControladoraPersistencia contPersist = new ControladoraPersistencia();
+        ControladoraLogica cont = new ControladoraLogica();
+        cont.createAlumno(new Alumno("Juan", "Perez", new Date()));
     }
 }
